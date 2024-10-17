0a644f81b6fb1dfceaf43747f0a2d09dd7f42dfe
Mikhail Tagirov <dev.mikhail.tagirov@outlook.com>
2024-10-17 18:55:30 +0300

    allow unused Store trait
    Signed-off-by: Mikhail Tagirov <dev.mikhail.tagirov@outlook.com>

--- frame/support/procedural/src/pallet/expand/store_trait.rs
+++ frame/support/procedural/src/pallet/expand/store_trait.rs
@@ -38,6 +38,7 @@ pub fn expand_store_trait(def: &mut Def) -> proc_macro2::TokenStream {
 		&def.storages.iter().map(|storage| &storage.cfg_attrs).collect::<Vec<_>>();
 
 	quote::quote_spanned!(trait_store.span() =>
+		#[allow(unused)]
 		#trait_vis trait #trait_store {
 			#(
 				#(#storage_cfg_attrs)*
